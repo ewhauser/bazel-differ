2a56b98f420413f22ba40c333a9b8fce9bcc35f8
Bret Walker <bretwalker@users.noreply.github.com>
2023-07-18 10:30:57 -0400

    Verbose output

--- internal/git_client.go
+++ internal/git_client.go
@@ -5,7 +5,7 @@ import (
 )
 
 type GitClient interface {
-	Checkout(hash string) string, error
+	Checkout(hash string) (string, error)
 }
 
 type gitClient struct {
@@ -18,8 +18,8 @@ func NewGitClient(dir string) GitClient {
 	}
 }
 
-func (g gitClient) Checkout(hash string) string, error {
+func (g gitClient) Checkout(hash string) (string, error) {
 	cmd := exec.Command("git", "-C", g.dir, "checkout", hash, "--quiet")
 	output, err := cmd.CombinedOutput()
-	return err
+	return string(output), err
 }
